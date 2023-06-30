8f7a266d7cce39c379774ce6883dae1c1e734a2b
Sierra Lopez <sierralopez1006@gmail.com>
2023-06-30 12:59:31 -0700

    added components needed and working on NavBar

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Container from 'react-bootstrap/Container'
+
+function Header() {
+    return(
+        <Container>
+            <NavBar/>
+        </Container>
+    )
+}
+
+export default Header
